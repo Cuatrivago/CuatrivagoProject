8a50f565e17de0b050da58660019f7bc46d4e1b2
Michael <michael.melendezm@gmail.com>
2017-04-10 11:59:47 -0600

    Se agregaron todas las vistas

---
+++ CuatrivagoProject/Controllers/DetailRoomController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CuatrivagoProject.Controllers
+{
+    public class DetailRoomController : Controller
+    {
+        // GET: DetailRoom
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DetailRoom/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DetailRoom/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DetailRoom/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DetailRoom/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DetailRoom/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DetailRoom/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DetailRoom/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
