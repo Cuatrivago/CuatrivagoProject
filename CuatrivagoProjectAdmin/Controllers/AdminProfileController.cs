e01dfec34ca0d038cf544e48ea8d117b26d97ebb
Kevin Jiménez <Kevin Jimnez>
2017-04-27 02:00:48 -0600

    Se añade el cerrar sesion al layout del modulo administrativo (Funcional)

--- CuatrivagoProjectAdmin/Controllers/AdminProfileController.cs
+++ CuatrivagoProjectAdmin/Controllers/AdminProfileController.cs
@@ -29,7 +29,7 @@ namespace CuatrivagoProjectAdmin.Controllers
                     //Este else retorna al index de logueo con un -2, el -2 le dice al index que este usuario intentó
                     //  entrar de forma no autorizada al modulo y se le presentará un mensaje sobre eso.
                     Request.Cookies["Admin"]["adminId"] = "-2";
-                    return View("~/Views/Login/Index.cshtml");
+                    return RedirectToAction("Index", "Login");
             } else
             {
                 //Este else es en caso de que la cookie no exista, se crea y se le da el codigo -2 para devolverlo a
@@ -39,7 +39,7 @@ namespace CuatrivagoProjectAdmin.Controllers
                 galleta.Expires = DateTime.Now.AddMinutes(1);
 
                 Response.Cookies.Add(galleta);
-                return View("~/Views/Login/Index.cshtml");
+                return RedirectToAction("Index", "Login");
             }
         }
 
