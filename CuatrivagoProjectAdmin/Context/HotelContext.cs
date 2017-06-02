9e930cae5dba88774176293b2d791ae45a49a1c9
Edwin Navarro Barahona <ednavarro@outlook.com>
2017-06-01 22:36:14 -0600

    update hotel

--- CuatrivagoProjectAdmin/Context/HotelContext.cs
+++ CuatrivagoProjectAdmin/Context/HotelContext.cs
@@ -11,24 +11,16 @@ namespace CuatrivagoProjectAdmin.Context
 {
     public class HotelContext
     {
-        public Hotel getInformationHotel(string conn) {
-            
+        public Hotel getInformationHotel(string conn)
+        {
             SqlConnection connection = new SqlConnection(conn);
-
-
-
             string sqlStoredProcedure = "SP_Retrieve_Hotel";
             SqlCommand cmdHotel = new SqlCommand(sqlStoredProcedure, connection);
-
             cmdHotel.CommandType = CommandType.StoredProcedure;
-
-            cmdHotel.Parameters.Add(new SqlParameter("@Hotel",1));
-
+            cmdHotel.Parameters.Add(new SqlParameter("@Hotel", 1));
             cmdHotel.Connection.Open();
             cmdHotel.ExecuteNonQuery();
-
             SqlDataReader reader = cmdHotel.ExecuteReader();
-
             Hotel hotel = new Hotel();
             reader.Read();
             hotel.name = reader.GetString(0);
@@ -38,13 +30,91 @@ namespace CuatrivagoProjectAdmin.Context
             hotel.phone = reader.GetString(4);
             hotel.postMail = reader.GetString(5);
             hotel.email = reader.GetString(6);
+            cmdHotel.Connection.Close();
+            return hotel;
+        }
+        public void updateHotel(Hotel hotel, string conn)
+        {
+
+            SqlConnection connection = new SqlConnection(conn);
+            string sqlStoredProcedure = "SP_Update_Hotel";
+            SqlCommand cmdHotel = new SqlCommand(sqlStoredProcedure, connection);
+            cmdHotel.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter id = new SqlParameter( "@ID",SqlDbType.Int);
+            //id.ParameterName =;
+            //id.SqlDbType = ;
+            id.Direction = ParameterDirection.Input;
+            id.Value = hotel.idHotel;
+            cmdHotel.Parameters.Add(id);
+
+
+            SqlParameter name = new SqlParameter("@Name",SqlDbType.Char,100);
+            //name.ParameterName = ;
+            //name.SqlDbType = ;
+            name.Direction = ParameterDirection.Input;
+            name.Value = hotel.name;
+            cmdHotel.Parameters.Add(name);
+
+
+            SqlParameter home = new SqlParameter("@HomeInformation",SqlDbType.Char,2000);
+            //home.ParameterName = ;
+            //home.SqlDbType = ;
+            home.Direction = ParameterDirection.Input;
+            home.Value = hotel.homeInformation;
+            cmdHotel.Parameters.Add(home);
+
+
+            SqlParameter about = new SqlParameter("@AboutUs",SqlDbType.Char,2000);
+            //about.ParameterName = "";
+            //about.SqlDbType = ;
+            about.Direction = ParameterDirection.Input;
+            about.Value = hotel.aboutUs;
+            cmdHotel.Parameters.Add(about);
+
+
+            SqlParameter add = new SqlParameter("@Address",SqlDbType.Char,500);
+            //add.ParameterName = ;
+            //add.SqlDbType = ;
+            add.Direction = ParameterDirection.Input;
+            add.Value = hotel.address_;
+            cmdHotel.Parameters.Add(add);
+
+
+            SqlParameter phone = new SqlParameter("@Phone",SqlDbType.Int);
+            //phone.ParameterName = ;
+            //phone.SqlDbType = ;
+            phone.Direction = ParameterDirection.Input;
+            phone.Value = hotel.phone;
+            cmdHotel.Parameters.Add(phone);
+
+
+            SqlParameter mail = new SqlParameter("@MailPost",SqlDbType.Char,50);
+            //mail.ParameterName = ;
+            //mail.SqlDbType = ;
+            mail.Direction = ParameterDirection.Input;
+            mail.Value = hotel.postMail;
+            cmdHotel.Parameters.Add(mail);
+
+
+            SqlParameter parameter = new SqlParameter("@Email", SqlDbType.Char,200);
+            //parameter.ParameterName = ;
+            //parameter.SqlDbType =;
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Value = hotel.email;
+            cmdHotel.Parameters.Add(parameter);
+
+            SqlParameter parameter2 = new SqlParameter("@R",SqlDbType.Char,1);
+            //parameter2.ParameterName = ;
+            //parameter2.SqlDbType = ;
+            parameter2.Direction = ParameterDirection.Output;
+
+            cmdHotel.Parameters.Add(parameter2);
+            cmdHotel.Connection.Open();
+            cmdHotel.ExecuteNonQuery();
 
             cmdHotel.Connection.Close();
 
-            return hotel;
-
-
         }
-
     }
 }
---
+++ CuatrivagoUnitTest/Test/HotelTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CuatrivagoProjectAdmin.Models;
+using CuatrivagoProjectAdmin.Context;
+
+namespace CuatrivagoUnitTest.Test
+{
+    [TestClass]
+    public class HotelTest
+    {
+
+        private HotelContext rc = new HotelContext();
+        string conn = "workstation id=CuatriVago.mssql.somee.com;packet size=4096;user id=cuatrivago;pwd=Cu@Tr0_V@G05;data source=CuatriVago.mssql.somee.com;persist security info=False;initial catalog=CuatriVago";
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Hotel newHotel = new Hotel();
+            newHotel.idHotel = 1;
+            newHotel.name = "Cuatrivago";
+            newHotel.address_ = "Costa Rica";
+            newHotel.phone = "22345678";
+            newHotel.email = "cuatri@cuatrivago.com";
+            newHotel.postMail = "catrivago@cuatrivago.com";
+            newHotel.aboutUs = "Durante años hemos trabajado en estrecha colaboración con organizaciones gubernamentales para implementar programas que beneficien a nuestro medio ambiente natural y nos permitan colaborar en la protección de la belleza natural de la región. Ejemplo de ello es la reconocida Certificación para la Sostenibilidad Turística.";
+            newHotel.homeInformation = "Como líder global en el negocio de la hospitalidad, en el Hotel Cuatrivago  reconocemos nuestra responsabilidad de proteger el medio ambiente en la ejecución de nuestra labor.  Estamos comprometidos a realizar todas las acciones necesarias para ser un hotel sostenible y responsable con nuestra comunidad.";
+            rc.updateHotel(newHotel, conn);
+        }
+}
+}
