8b9204852fb52367d5b992eb74c29a86d0818e4f
Edwin Navarro Barahona <ednavarro@outlook.com>
2017-06-30 21:47:35 -0600

    topo publicidad

--- CuatrivagoProject/Context/AdvertisementContext.cs
+++ CuatrivagoProject/Context/AdvertisementContext.cs
@@ -11,13 +11,15 @@ namespace CuatrivagoProject.Context
 {
     public class AdvertisementContext
     {
+        string connUCR = WebConfigurationManager.ConnectionStrings["connectionUCR"].ToString();
+
         public List<Advertisement> getInformationAdvertisement(string conn)
         {
 
 
             SqlConnection connection = new SqlConnection(conn);
 
-            string sqlStoredProcedure = "SP_Retrieve_Advertisement_All";
+            string sqlStoredProcedure = "SP_Retrieve_Advertisement_All_ID";
             SqlCommand cmdAdvertisement = new SqlCommand(sqlStoredProcedure, connection);
 
             cmdAdvertisement.CommandType = CommandType.StoredProcedure;
@@ -35,10 +37,20 @@ namespace CuatrivagoProject.Context
                 {
                     Advertisement e_advertisement = new Advertisement();
 
-                    e_advertisement.name = reader.GetString(0);
-                    e_advertisement.description = reader.GetString(1);
-                    e_advertisement.url = reader.GetString(2);
-                    e_advertisement.path = reader.GetString(3);
+                    //e_advertisement.name = reader.GetString(0);
+                    //e_advertisement.description = reader.GetString(1);
+                    //e_advertisement.url = reader.GetString(2);
+                    ////e_advertisement.path = reader.GetString(3);
+
+                    e_advertisement.idAdvertisement = reader.GetInt32(0);
+                    e_advertisement.name = reader.GetString(1);
+                    e_advertisement.description = reader.GetString(2);
+                    e_advertisement.url = reader.GetString(3);
+
+                    ImageContext context = new ImageContext();
+
+
+                    e_advertisement.image = context.getImageByType(connUCR, e_advertisement.idAdvertisement, 'A');
 
                     advertisement.Add(e_advertisement);
                   
